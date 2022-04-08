323f5a74158b4d89e2cb143a6d020051eb6c82b7
lucasantos-S <lucassantos.dsilv@gmail.com>
2022-04-07 22:04:21 -0300

    alterando a posição do animationScroll

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 import ScrollSuave from "./modules/scroll-suave.js";
-import animationScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js";
 import initModal from "./modules/modal.js";
@@ -8,6 +7,7 @@ import initDropdownMenu from "./modules/dropdown-menu.js";
 import initMenuMobile from "./modules/menu-mobile.js";
 import fecthAnimais from "./modules/fetch-animais.js";
 import FecthBiticoin from "./modules/fetch-bitcoin.js";
+import animationScroll from "./modules/scroll-animacao.js";
 
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]')
 scrollSuave.init()
