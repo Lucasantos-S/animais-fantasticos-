1b26c3614ea94f76a910d7a09b5ddfc88bfa21c0
lucasantos-S <lucassantos.dsilv@gmail.com>
2022-04-08 20:58:55 -0300

    refatorando modal com class

--- js/modules/modal.js
+++ js/modules/modal.js
@@ -1,25 +1,43 @@
-export default function initModal() {
-  const botaoAbrir = document.querySelector('[data-modal="abrir"]');
-  const botaoFechar = document.querySelector('[data-modal="fechar"]');
-  const containerModal = document.querySelector('[data-modal="container"]');
+export default class initModal {
+  constructor(open, exit, container) {
+    this.botaoAbrir = document.querySelector(open);
+    this.botaoFechar = document.querySelector(exit);
+    this.containerModal = document.querySelector(container);
+
+    this.eventToggleModal = this.eventToggleModal.bind(this);
+    this.clickForaModal = this.clickForaModal.bind(this);
+  }
 
   //toggle vai adicionar ou tirar a classe ativo do modal
 
-  function toogleModal(event) {
+  toogleModal() {
+    this.containerModal.classList.toggle("ativo");
+  }
+
+  eventToggleModal(event) {
+    console.log(this);
     event.preventDefault();
-    containerModal.classList.toggle("ativo");
+    this.toogleModal();
   }
 
   //fechar quando ocorrer o click fora do this containerModal
 
-  function clickForaModal(event) {
-    console.log(this);
-    console.log(this);
-    if (event.target === this) toogleModal(event);
+  clickForaModal(event) {
+    if (event.target === this.containerModal) {
+      this.toogleModal(event);
+    }
   }
-  if (botaoAbrir && botaoFechar && containerModal) {
-    botaoAbrir.addEventListener("click", toogleModal);
-    botaoFechar.addEventListener("click", toogleModal);
-    containerModal.addEventListener("click", clickForaModal);
+
+  addModalEvent() {
+    this.botaoAbrir.addEventListener("click", this.eventToggleModal);
+    this.botaoFechar.addEventListener("click", this.eventToggleModal);
+    this.containerModal.addEventListener("click", this.clickForaModal);
+  }
+
+  init() {
+    if (this.botaoAbrir && this.botaoFechar && this.containerModal) {
+      this.addModalEvent();
+    }
+    return this;
   }
 }
