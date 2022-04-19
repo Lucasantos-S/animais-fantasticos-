ba7d462392a2fefe92f58e42e19673dc5fe650eb
lucasantos-S <lucassantos.dsilv@gmail.com>
2022-04-18 21:07:28 -0300

    dropdown-menu refatorado

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,18 +1,37 @@
-import outsideClick from"./outsideclick.js";
+import outsideClick from "./outsideclick.js";
 
-export default function initDropdownMenu() {}
+export default class DropdownMenu {
+  constructor(menu) {
+    this.dropownMenu = document.querySelectorAll(menu);
+    this.activeClass = 'ativo'
+    this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+  }
 
-const dropownMenu = document.querySelectorAll("[data-dropdown");
+  //ativa o dropdrownmenu e adiciona
+  //a função que observa o click fora dele
 
-function handleClick(event) {
-  event.preventDefault();
-  this.classList.add("ativo");
-  outsideClick(this, ["touchstart", "click"], () => {
-    this.classList.remove("ativo");
-  });
+  activeDropdownMenu(event) {
+    event.preventDefault();
+    const element = this.event.currentTarget;
+    element.classList.add(this.activeClass);
+    outsideClick(element, ["touchstart", "click"], () => {
+      element.classList.remove(this.activeClass);
+    });
+  }
+
+  //adiciona os eventos no menu e passa a funcção de ativar 
+  addDropdownMenuEvent() {
+    this.dropownMenu.forEach((menu) => {
+      ["touchstart", "click"].forEach((userEvent) => {
+        menu.addEventListener(userEvent, this.activeDropdownMenu);
+      });
+    });
+  }
+
+  init() {
+    if (this.dropownMenu.length) {
+      this.addDropdownMenuEvent();
+    }
+    return this;
+  }
 }
-dropownMenu.forEach((menu) => {
-  ["touchstart", "click"].forEach((userEvent) => {
-    menu.addEventListener(userEvent, handleClick);
-  });
-})
